5f2ffef14ac490d91d3705c2b522b0a67b33e189
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-09 10:16:14 +0000

    add validate_image method to PostSerializer to prevent users uploading files that are too large

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,8 +9,29 @@ class PostSerializer(models.Model):
     profile_image = models.ReadOnlyField(source="author.profile.image.id")
 
     def get_is_author(self, obj):
+        """
+        Checks if the current user is the author of the post
+        """
         request = self.context['request']
         return request.user == obj.author
+
+    # validate_image function used from Code Institute's Moments Project
+    def validate_image(self, value):
+        """
+        Validates the post image so that images that are too large can't be
+        uploaded
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
 
     class Meta:
         model = Post
