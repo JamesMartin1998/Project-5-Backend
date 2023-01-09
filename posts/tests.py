f22f433513c39c1d9b3d32b5a61ab9ee739dc1ae
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-09 16:32:28 +0000

    add Post unit tests

--- posts/tests.py
+++ posts/tests.py
@@ -13,8 +13,6 @@ class PostListViewTests(APITestCase):
         Post.objects.create(author=james, title="test post")
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
-        print(len(response.data))
 
     def test_logged_in_user_can_create_post(self):
         self.client.login(username='james', password='fakepassword')
@@ -33,4 +31,69 @@ class PostListViewTests(APITestCase):
         })
         count = Post.objects.count()
         self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        james = User.objects.create_user(
+            username='james',
+            password="fakepassword"
+        )
+        aram = User.objects.create_user(
+            username='aram',
+            password='fakepassword'
+        )
+        Post.objects.create(
+            author=james,
+            title='test',
+            category="sport"
+        )
+        Post.objects.create(
+            author=aram,
+            title='test2',
+            category="people"
+        )
+
+    def test_user_can_retrieve_post_by_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'test')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_retrieve_post_by_invalid_id(self):
+        response = self.client.get('/posts/9999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_post_if_author(self):
+        self.client.login(username='james', password='fakepassword')
+        response = self.client.put(
+            '/posts/1/',
+            {'title': 'testupdated', 'category': 'people'}
+        )
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'testupdated')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_update_post_if_not_author(self):
+        self.client.login(username='james', password='fakepassword')
+        response = self.client.put(
+            '/posts/2/',
+            {'title': 'testupdated', 'category': 'people'}
+        )
+        post = Post.objects.filter(pk=2).first()
+        # self.assertEqual(post.title, 'testupdated')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_delete_post_if_author(self):
+        self.client.login(username='james', password='fakepassword')
+        response = self.client.delete(
+            '/posts/1/'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_cannot_delete_post_if_not_author(self):
+        self.client.login(username='james', password='fakepassword')
+        response = self.client.delete(
+            '/posts/2/'
+        )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
