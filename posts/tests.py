2f339a4756361ff92ae9c605a9f8818261ff8ca3
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-09 12:12:55 +0000

    add PostListViewTests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='james', password="fakepassword")
+
+    def test_can_list_posts(self):
+        james = User.objects.get(username='james')
+        Post.objects.create(author=james, title="test post")
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='james', password='fakepassword')
+        response = self.client.post('/posts/', {
+            'title': 'a title',
+            'category': 'sport'
+        })
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_cannot_create_post(self):
+        response = self.client.post('/posts/', {
+            'title': 'a title',
+            'category': 'sport'
+        })
+        count = Post.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
