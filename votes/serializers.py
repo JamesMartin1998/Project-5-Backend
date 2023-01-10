d68a269553b4ba5a64aa23649f73aa17c83d827b
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-10 15:39:10 +0000

    add create method to vote serializer to prevent users from adding multiple votes to the same post

--- votes/serializers.py
+++ votes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from votes.models import Vote
 
@@ -8,3 +9,16 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
         fields = ['id', 'created_at', 'owner', 'post']
+
+    # create method used from Code Institute's Moments Project
+    # https://github.com/Code-Institute-Solutions/drf-api/blob/a918da6065ef5c399ff7655638960c2628af83d4/likes/serializers.py
+    def create(self, validated_data):
+        """
+        Prevents users from voting multiple times on a post
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
